f884d03cff4dcab6398b8dc62a6c7454919c32ad
Riiduu <100873979+Riiduu@users.noreply.github.com>
2022-11-20 15:40:02 +0200

    Added a search icon to the input and also removed useless borders on input

--- src/components/HeaderSearch.jsx
+++ src/components/HeaderSearch.jsx
@@ -1,18 +1,21 @@
 import React from "react";
+import SearchIcon from '@mui/icons-material/Search';
 
 const SearchBar = ({ onSearch }) => {
 
     const onSearchbarChange = (event) => {
         onSearch(event.target.value);
-    } 
+    }
 
     return (
         <div className="note-search">
-            <input 
-                type="text" 
+            <SearchIcon id='search-icon' />
+            <input
+                type="text"
                 placeholder="Search for notes..."
                 onChange={onSearchbarChange}
             />
+
         </div>
     )
 }
